4178f32946149d58bbe9b3f556b1724cc40df4e6
satendra <satendra.pal@mawaimail.com>
2024-06-06 11:35:04 +0530

    changes

---
+++ server/model/books.js
@@ -0,0 +1,13 @@
+const {pool} = require('pg');
+const Books = new Schema({
+    title: String, // String is shorthand for {type: String}
+    author: String,
+    body: String,
+    comments: [{ body: String, date: Date }],
+    date: { type: Date, default: Date.now },
+    hidden: Boolean,
+    meta: {
+      votes: Number,
+      favs: Number
+    }
+  });
